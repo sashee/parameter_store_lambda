629ed6150eb74ccea3ccd907eaa907c51384b2af
Tamas Sallai <gsashee@gmail.com>
2020-10-29 09:44:22 +0100

    remove API gateway

--- main.tf
+++ main.tf
@@ -85,22 +85,6 @@ resource "aws_iam_role" "lambda_exec" {
 EOF
 }
 
-# API Gateway
-
-resource "aws_apigatewayv2_api" "api" {
-  name          = "api-${random_id.id.hex}"
-  protocol_type = "HTTP"
-  target        = aws_lambda_function.lambda.arn
-}
-
-resource "aws_lambda_permission" "apigw" {
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.lambda.arn
-  principal     = "apigateway.amazonaws.com"
-
-  source_arn = "${aws_apigatewayv2_api.api.execution_arn}/*/*"
-}
-
-output "url" {
-  value = aws_apigatewayv2_api.api.api_endpoint
+output "function" {
+	value = aws_lambda_function.lambda.arn
 }
